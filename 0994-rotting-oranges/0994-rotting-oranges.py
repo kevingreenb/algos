36bcd56a1daafc81288a7c608d84f1e58736a395
Kevin Green <kevin.e.green@hotmail.com>
2025-11-25 21:10:44 -0600

    Time: 5 ms (33.9%), Space: 17.9 MB (25.77%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -15,22 +15,21 @@ class Solution:
         if oranges == 0:
             return 0
 
-        direction = [(1, 0), (-1, 0), (0,1), (0, -1)]
+        direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
         while q:
             size = len(q)
-            ans += 1
+            ans += 1 
             for _ in range(size):
                 r, c = q.popleft()
+                
                 for r2, c2 in direction:
-                    if r+r2 < len(grid) and r+r2 >= 0 and grid[r+r2][c] == 1:
-                        grid[r+r2][c] = 2
-                        q.append((r+r2, c))
-                        rotten += 1
-                    if c+c2 < len(grid[0]) and c+c2 >= 0 and grid[r][c+c2] == 1:
-                        grid[r][c+c2] = 2
-                        q.append((r, c+c2))
-                        rotten += 1
-
+                    nr, nc = r + r2, c + c2
+                    if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])):
+                        if grid[nr][nc] == 1:
+                            grid[nr][nc] = 2
+                            q.append((nr, nc))
+                            rotten += 1
+                            
         return ans if oranges == rotten else -1
         
