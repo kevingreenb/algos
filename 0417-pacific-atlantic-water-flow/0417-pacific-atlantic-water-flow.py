c179e82f145e9a7a99c89e92595f0784c3d8a880
Kevin Green <kevin.e.green@hotmail.com>
2025-11-27 10:42:28 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,18 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        def dfs(r, c, heights, visited, previous):
+        pac, atl = set(), set()
+
+        def explore(r, c, heights, visited, prev):
             if (
                 r < 0
                 or c < 0
                 or r >= len(heights)
                 or c >= len(heights[r])
-                or (r, c) in visited
-                or heights[r][c] < previous
+                or heights[r][c] <= prev
+                or heights[r][c] in visited
             ):
                 return
-            visited.add((r,c))
-            dfs(r - 1, c, heights, visited, heights[r][c])
-            dfs(r + 1, c, heights, visited, heights[r][c])
-            dfs(r, c - 1, heights, visited, heights[r][c])
-            dfs(r, c + 1, heights, visited, heights[r][c])
+            visited.add((r, c))
+            explore(r, c, heights, visited, prev)
+            explore(r, c, heights, visited, prev)
 
-        atl, pac = set(), set()
-        n, m = len(heights), len(heights[0])
-
-        for r in range(n):
-            dfs(r, 0, heights, pac, heights[r][0])
-            dfs(r, m - 1, heights, atl, heights[r][m - 1])
-
-        for c in range(m):
-            dfs(0, c, heights, pac, heights[0][c])
-            dfs(n - 1, c, heights, atl, heights[n - 1][c])
-
-        ans = []
-        for cell in pac:
-            if cell in atl:
-                ans.append(cell)
-        
-        return ans
