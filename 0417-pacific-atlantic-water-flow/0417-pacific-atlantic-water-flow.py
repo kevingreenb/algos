8104b8ec1fa001ea81164686de075e5dbe1cb1c2
Kevin Green <kevin.e.green@hotmail.com>
2025-11-27 10:43:54 -0600

    Time: 29 ms (64.98%), Space: 19.1 MB (90.41%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,6 +1,8 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         pac, atl = set(), set()
+        ans = []
+        n, m = len(heights), len(heights[0])
 
         def explore(r, c, heights, visited, prev):
             if (
@@ -8,11 +10,26 @@ class Solution:
                 or c < 0
                 or r >= len(heights)
                 or c >= len(heights[r])
-                or heights[r][c] <= prev
-                or heights[r][c] in visited
+                or heights[r][c] < prev
+                or (r, c) in visited
             ):
                 return
             visited.add((r, c))
-            explore(r, c, heights, visited, prev)
-            explore(r, c, heights, visited, prev)
+            explore(r - 1, c, heights, visited, heights[r][c])
+            explore(r + 1, c, heights, visited, heights[r][c])
+            explore(r, c - 1, heights, visited, heights[r][c])
+            explore(r, c + 1, heights, visited, heights[r][c])
+        
+        for r in range(n):
+            explore(r, 0, heights, pac, heights[r][0])
+            explore(r, m-1, heights, atl, heights[r][m-1])
 
+        for c in range(m):
+            explore(0, c, heights, pac, heights[0][c])
+            explore(n-1, c, heights, atl, heights[n-1][c])
+        
+        for element in pac:
+            if element in atl:
+                ans.append([element[0],element[1]])
+        
+        return ans
