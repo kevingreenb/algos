4e4e86f680b13f72a216785f506fe5eda919a8b7
Kevin Green <kevin.e.green@hotmail.com>
2025-11-27 08:39:45 -0600

    Time: 224 ms (96.07%), Space: 20.3 MB (57.12%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,25 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def explore(r, c, grid):
-            if (
-                r < 0
-                or c < 0
-                or r >= len(grid)
-                or c >= len(grid[r])
-                or grid[r][c] != "1"
-            ):
-                return 0
+            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[0]) or grid[r][c] != "1":
+                return
+
             grid[r][c] = "0"
-            explore(r - 1, c, grid)
+
             explore(r + 1, c, grid)
-            explore(r, c - 1, grid)
+            explore(r - 1, c, grid)
             explore(r, c + 1, grid)
+            explore(r, c - 1, grid)
 
         ans = 0
+
         for r in range(len(grid)):
             for c in range(len(grid[r])):
                 if grid[r][c] == "1":
                     ans += 1
                     explore(r, c, grid)
-                    
+
         return ans
+        
