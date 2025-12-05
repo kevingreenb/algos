de23cc63ff9cea8945d987740cb7932144b8fda3
Kevin Green <kevin.e.green@hotmail.com>
2025-12-05 11:15:05 -0600

    Time: 7 ms (89.9%), Space: 20.4 MB (36.51%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,9 +4,15 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         heap = []
         
+        # push (value, index, node) so ties on value are resolved by index
         for i, node in enumerate(lists):
             if node:
                 heapq.heappush(heap, (node.val, i, node))
@@ -18,7 +24,8 @@ class Solution:
             ptr.next = node
             ptr = ptr.next
             if node.next:
+                # use the same index or a new unique counter; using i is fine here
                 heapq.heappush(heap, (node.next.val, i, node.next))
         
         return dummy.next
-        
+              
