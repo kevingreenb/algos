6785ac3373415494a6976bac9b1148a606bd5e90
Kevin Green <kevin.e.green@hotmail.com>
2025-12-10 11:29:42 -0600

    Time: 0 ms (100%), Space: 18.1 MB (20.57%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         ans = []
-        def backtrack(i, cur):
+        def helper(i, cur):
             if i == len(nums):
                 ans.append(cur[:])
                 return
             cur.append(nums[i])
-            backtrack(i+1,cur)
+            helper(i+1,cur)
             cur.pop()
-            backtrack(i+1,cur)
-        backtrack(0,[])
+            helper(i+1,cur)
+
+        helper(0, [])
         return ans
+        
