e68ffe42a45c1c788edb5e4f71b8110710be23cb
Kevin Green <kevin.e.green@hotmail.com>
2025-12-13 08:04:26 -0600

    Time: 47 ms (66.86%), Space: 34.6 MB (44.87%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,25 +1,18 @@
 class Solution:
-
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        for l in range(1, n + 1):
-            for i in range(n - l + 1):
-                dp[i][i + l - 1] = (s[i] == s[i + l - 1] and
-                                    (i + 1 > (i + l - 2) or
-                                    dp[i + 1][i + l - 2]))
+        ans, cur = [], []
+        def is_palin(s2):
+            return s2 == s2[::-1]
 
         def dfs(i):
-            if i >= n:
-                return [[]]
-
-            ret = []
-            for j in range(i, n):
-                if dp[i][j]:
-                    nxt = dfs(j + 1)
-                    for part in nxt:
-                        cur = [s[i : j + 1]] + part
-                        ret.append(cur)
-            return ret
-
-        return dfs(0)
+            if i >= len(s):
+                ans.append(cur[:])
+                return
+            for j in range(i, len(s)):
+                s2 = s[i : j + 1]
+                if is_palin(s2):
+                    cur.append(s2)
+                    dfs(j + 1)
+                    cur.pop()
+        dfs(0)
+        return ans
