9d654293ba77bd659abf2c0a53f0415649730cfe
Kevin Green <kevin.e.green@hotmail.com>
2025-12-17 09:25:42 -0600

    Time: 7 ms (72.39%), Space: 18 MB (13.68%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -7,11 +7,11 @@ class Solution:
             if s == target:
                 ans.append(cur[:])
                 return
-            elif s > target:
-                return
             for j in range(i, len(candidates)):
                 if j > i and candidates[j] == candidates[j-1]:
                     continue
+                if s + candidates[j] > target:
+                    break
                 cur.append(candidates[j])
                 backtrack(j + 1, s + candidates[j])
                 cur.pop()
