fd74e4e5d2a94aa047812a12cf312fca6f52f53e
Kevin Green <kevin.e.green@hotmail.com>
2025-12-18 15:23:56 -0600

    Time: 11 ms (70.28%), Space: 17.7 MB (99.99%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,33 +1,30 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         cols = set()
-        p_diag = set()
-        n_diag = set()
+        pos_diag = set()
+        neg_diag = set()
         ans = []
         board = [["."] * n for _ in range(n)]
 
-        def backtrack(row):
+        def helper(row):
             if row == n:
                 copy = ["".join(r) for r in board]
                 ans.append(copy)
                 return
             for c in range(n):
-                p_diag2 = row + c
-                n_diag2 = row - c
-                if c in cols or p_diag2 in p_diag or n_diag2 in n_diag:
+                pos_diag2 = row + c
+                neg_diag2 = row - c
+                if c in cols or pos_diag2 in pos_diag or neg_diag2 in neg_diag:
                     continue
-
                 cols.add(c)
-                p_diag.add(p_diag2)
-                n_diag.add(n_diag2)
+                pos_diag.add(pos_diag2)
+                neg_diag.add(neg_diag2)
                 board[row][c] = "Q"
-
-                backtrack(row + 1)
-
+                helper(row + 1)
                 cols.remove(c)
-                p_diag.remove(p_diag2)
-                n_diag.remove(n_diag2)
+                pos_diag.remove(pos_diag2)
+                neg_diag.remove(neg_diag2)                
                 board[row][c] = "."
 
-        backtrack(0)
+        helper(0)
         return ans
