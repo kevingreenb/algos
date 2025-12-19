dc4bfacf2ab76459da63d621417b9dcb2f2e2d15
Kevin Green <kevin.e.green@hotmail.com>
2025-12-19 07:45:54 -0600

    Time: 16 ms (88.3%), Space: 19.1 MB (89.83%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,19 +1,10 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # Count frequency of each task
         counts = Counter(tasks)
         max_freq = max(counts.values())
-        
-        # Count how many tasks have that maximum frequency
-        max_freq_count = 0
-        for task in counts:
-            if counts[task] == max_freq:
-                max_freq_count += 1
-                
-        # Calculate the formulaic result
-        ans = (max_freq - 1) * (n + 1) + max_freq_count
-        
-        # If we have many tasks and few idle slots, the length
-        # could actually be longer than the formula (no idle time).
-        # In that case, the answer is just the total number of tasks.
+        max_freq_counts = 0
+        for v in counts.values():
+            if v == max_freq:
+                max_freq_counts += 1
+        ans = (max_freq - 1) * (n + 1) + max_freq_counts
         return max(ans, len(tasks))
