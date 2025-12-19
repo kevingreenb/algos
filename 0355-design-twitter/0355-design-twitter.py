23e8b429cb9d6ef81fc3a81184d16d179814dc34
Kevin Green <kevin.e.green@hotmail.com>
2025-12-19 17:25:04 -0600

    Time: 11 ms (74.91%), Space: 26.6 MB (88.39%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,39 +1,35 @@
 class Twitter:
-
     def __init__(self):
+        self.follows = defaultdict(set)
+        self.users = defaultdict(list)
         self.time = 0
-        self.tweets = defaultdict(list)
-        self.following = defaultdict(set)
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.time -= 1 
-        self.tweets[userId].append((self.time, tweetId))
+        self.time -= 1
+        self.users[userId].append((self.time, tweetId))
 
     def getNewsFeed(self, userId: int) -> List[int]:
-        heap = []
-        self.following[userId].add(userId)
-        for followee in self.following[userId]:
-            if self.tweets[followee]:
-                time, tweetId = self.tweets[followee][-1]
-                idx = len(self.tweets[followee]) - 1
-                heapq.heappush(heap, (time, tweetId, followee, idx))
+        ans = []
+        self.follows[userId].add(userId)
+        h = []
 
-        res = []
-        while heap and len(res) < 10:
-            time, tweetId, user, idx = heapq.heappop(heap)
-            res.append(tweetId)
-            if idx > 0:
-                next_time, next_tweet = self.tweets[user][idx - 1]
-                heapq.heappush(
-                    heap,
-                    (next_time, next_tweet, user, idx - 1)
-                )
-        return res
+        for followee in self.follows[userId]:
+            if self.users[followee]:
+                i = len(self.users[followee]) - 1
+                heapq.heappush(h, (self.users[followee][i][0], self.users[followee], i))
+
+        while h and len(ans) < 10:
+            time, cur, i = heapq.heappop(h)
+            ans.append(cur[i][1])
+
+            if i > 0:
+                i -= 1
+                heapq.heappush(h, (cur[i][0], cur, i))
+
+        return ans
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        if followerId != followeeId:
-            self.following[followerId].add(followeeId)
+        self.follows[followerId].add(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.following[followerId]:
-            self.following[followerId].remove(followeeId)
+        self.follows[followerId].discard(followeeId)
