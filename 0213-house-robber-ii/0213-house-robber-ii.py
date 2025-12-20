ae6a0bf383d73e9ba41ec448cb9f7ec279a24a86
Kevin Green <kevin.e.green@hotmail.com>
2025-12-20 17:18:14 -0600

    Time: 0 ms (100%), Space: 17.3 MB (99.42%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,24 +3,16 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-
-
         def rob_linear(houses):
             if not houses:
                 return 0
             if len(houses) == 1:
                 return houses[0]
-            
             prev2 = houses[0]
             prev1 = max(houses[0], houses[1])
-            
             for i in range(2, len(houses)):
                 current = max(prev2 + houses[i], prev1)
                 prev2 = prev1
                 prev1 = current
-                
             return prev1
-        
-        # We run your logic twice: 
-        # once excluding the last house, once excluding the first.
         return max(rob_linear(nums[:-1]), rob_linear(nums[1:]))
