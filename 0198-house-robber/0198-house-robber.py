6f8046a9c4f6dcd11dc6b42c2bc9a61e3582e46a
Kevin Green <kevin.e.green@hotmail.com>
2025-12-20 13:24:34 -0600

    Time: 0 ms (100%), Space: 17.2 MB (99.97%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        prev2 = nums[0]
+        prev1 = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            current = max(prev2 + nums[i], prev1)
+            prev2 = prev1
+            prev1 = current
+        return prev1
+        
