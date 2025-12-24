f471ff790c45465668cf2973e45403f22627323f
Kevin Green <kevin.e.green@hotmail.com>
2025-12-23 19:48:49 -0600

    Time: 51 ms (49.2%), Space: 33.9 MB (81.28%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,18 +1,19 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         ans, cur = [], []
-        def is_palin(s2):
-            return s2 == s2[::-1]
-
-        def dfs(i):
-            if i >= len(s):
+        def is_valid(s2):
+            if s2 == s2[::-1]:
+                return True
+        def helper(i):
+            if i == len(s):
                 ans.append(cur[:])
                 return
             for j in range(i, len(s)):
-                s2 = s[i : j + 1]
-                if is_palin(s2):
+                s2 = s[i:j+1]
+                if is_valid(s2):
                     cur.append(s2)
-                    dfs(j + 1)
+                    helper(j + 1)
                     cur.pop()
-        dfs(0)
+        helper(0)
         return ans
+        
