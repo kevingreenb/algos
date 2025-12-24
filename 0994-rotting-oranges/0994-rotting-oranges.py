e4dee6aa4f7d22adf5b36a5063592b788cabf880
Kevin Green <kevin.e.green@hotmail.com>
2025-12-24 11:30:58 -0600

    Time: 4 ms (45.41%), Space: 17.3 MB (98.07%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,35 +1,30 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        oranges = 0
-        rotten = 0
-        ans = -1
+        oranges, ans = 0, 0
+        m, n = len(grid), len(grid[0])
         q = deque()
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
+        for r in range(m):
+            for c in range(n):
                 if grid[r][c] == 1:
                     oranges += 1
-                elif grid[r][c] == 2:
-                    q.append((r,c))
-                    
+                if grid[r][c] == 2:
+                    q.append((r, c))
+
         if oranges == 0:
             return 0
 
-        direction = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
         while q:
+            ans += 1
             size = len(q)
-            ans += 1 
             for _ in range(size):
                 r, c = q.popleft()
-                
-                for r2, c2 in direction:
-                    nr, nc = r + r2, c + c2
-                    if (0 <= nr < len(grid) and 0 <= nc < len(grid[0])):
-                        if grid[nr][nc] == 1:
-                            grid[nr][nc] = 2
-                            q.append((nr, nc))
-                            oranges -= 1
-                            
-        return ans if oranges == 0 else -1
-        
+                directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                        q.append((nr, nc))
+                        grid[nr][nc] = 2
+                        oranges -= 1
+
+        return ans - 1 if oranges == 0 else -1        
