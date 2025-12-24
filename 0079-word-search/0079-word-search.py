385c4cd2a71605bc0940dbfa15cfdb772afbc4a1
Kevin Green <kevin.e.green@hotmail.com>
2025-12-23 19:34:51 -0600

    Time: 3454 ms (70.86%), Space: 17.5 MB (95.72%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,30 +1,32 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m, n = len(board), len(board[0])
-        def explore(i, r, c):
-            if i >= len(word):
+        def explore(r, c, i):
+            if i == len(word):
                 return True
-    
-            if r < 0 or c < 0 or r >= m or c >= n or word[i] != board[r][c]:
+            if (
+                r < 0
+                or c < 0
+                or r >= len(board)
+                or c >= len(board[r])
+                or board[r][c] != word[i]
+            ):
                 return False
-
             temp = board[r][c]
             board[r][c] = "#"
-
             if (
-                explore(i + 1, r + 1, c) or
-                explore(i + 1, r - 1, c) or 
-                explore(i + 1, r, c + 1) or
-                explore(i + 1, r, c - 1)
+                explore(r + 1, c, i + 1)
+                or explore(r - 1, c, i + 1)
+                or explore(r, c + 1, i + 1)
+                or explore(r, c - 1, i + 1)
             ):
                 return True
-
+            
             board[r][c] = temp
             return False
-        
-        for r in range(m):
-            for c in range(n):
-                if board[r][c] == word[0] and explore(0, r, c):
-                    return True
-                    
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                if board[r][c] == word[0]:
+                    if explore(r, c, 0):
+                        return True
         return False
+            
