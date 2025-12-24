74a475609413c4ac25620b6309e151a85c66c53f
Kevin Green <kevin.e.green@hotmail.com>
2025-12-23 21:19:26 -0600

    Time: 11 ms (69.71%), Space: 17.8 MB (94.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,30 +1,28 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        cols = set()
-        pos_diag = set()
-        neg_diag = set()
+        cols, pos_d, neg_d = set(), set(), set()
+        board = [["." for _ in range(n)] for _ in range(n)]
         ans = []
-        board = [["."] * n for _ in range(n)]
-
-        def helper(row):
-            if row == n:
-                copy = ["".join(r) for r in board]
+        def helper(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
                 ans.append(copy)
                 return
-            for c in range(n):
-                pos_diag2 = row + c
-                neg_diag2 = row - c
-                if c in cols or pos_diag2 in pos_diag or neg_diag2 in neg_diag:
+            for c in range(len(board[r])):
+                if c in cols:
                     continue
+                elif r + c in pos_d:
+                    continue
+                elif r - c in neg_d:
+                    continue
+                board[r][c] = "Q"
                 cols.add(c)
-                pos_diag.add(pos_diag2)
-                neg_diag.add(neg_diag2)
-                board[row][c] = "Q"
-                helper(row + 1)
+                pos_d.add(r + c)
+                neg_d.add(r - c)
+                helper(r + 1)
+                board[r][c] = "."
                 cols.remove(c)
-                pos_diag.remove(pos_diag2)
-                neg_diag.remove(neg_diag2)                
-                board[row][c] = "."
-
+                pos_d.remove(r + c)
+                neg_d.remove(r - c)
         helper(0)
-        return ans
+        return ans                
