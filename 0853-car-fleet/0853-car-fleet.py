e60c0f0e2e7844102c46f81ad345ad9041c97e2e
Kevin Green <kevin.e.green@hotmail.com>
2025-12-26 18:45:35 -0600

    Time: 159 ms (82.59%), Space: 39.1 MB (89.82%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,14 +1,10 @@
 class Solution:
-    def carFleet(self, target, position, speed):
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = sorted(zip(position, speed), reverse=True)
-        
-        fleets = 0
-        prev_time = 0
-        
-        for pos, spd in cars:
-            time = (target - pos) / spd
-            if time > prev_time:
-                fleets += 1
-                prev_time = time
-        
-        return fleets
+        ans, prev = 0, 0
+        for p, s in cars:
+            time = (target-p)/s
+            if time > prev:
+                prev = time
+                ans += 1
+        return ans
