c0e419f4452018505e1ccf2007ce2f3a7c642efb
Kevin Green <kevin.e.green@hotmail.com>
2025-12-30 08:40:35 -0600

    Time: 102 ms (90.93%), Space: 17.6 MB (97.02%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,12 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        ans, left, most_freq = 0, 0, 0
         count = defaultdict(int)
-        ans, most_freq, left = 0, 0, 0
         for i, c in enumerate(s):
             count[c] += 1
             most_freq = max(most_freq, count[c])
-            while (i - left + 1 - most_freq) > k:
+            while i - left + 1 - most_freq > k:
                 count[s[left]] -= 1
                 left += 1
             ans = max(ans, i - left + 1)
         return ans
+        
