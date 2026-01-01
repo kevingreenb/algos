41401fff9a0bd202629e957397cca44212d5c0f8
Kevin Green <kevin.e.green@hotmail.com>
2026-01-01 11:04:35 -0600

    Time: 72 ms (57.86%), Space: 17.8 MB (96.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,26 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if not t or not s:
+        if len(t) > len(s):
             return ""
-        dict_t = Counter(t)
-        required = len(dict_t)
-        l, r = 0, 0
-        formed = 0
-        window_counts = {}
-        ans = float("inf"), None, None
-
-        while r < len(s):
-            char = s[r]
-            window_counts[char] = window_counts.get(char, 0) + 1
-            if char in dict_t and window_counts[char] == dict_t[char]:
-                formed += 1
-            while l <= r and formed == required:
-                char = s[l]
-                if r - l + 1 < ans[0]:
-                    ans = (r - l + 1, l, r)
-                window_counts[char] -= 1
-                if char in dict_t and window_counts[char] < dict_t[char]:
-                    formed -= 1
-                l += 1    
-            r += 1    
-        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
+        count_t = Counter(t)
+        count_s = defaultdict(int)
+        needed = len(count_t)
+        left = formed = 0
+        ans_len = float("inf")
+        ans = [0, 0]
+        for right, c in enumerate(s):
+            count_s[c] += 1
+            if c in count_t and count_t[c] == count_s[c]:
+                needed -= 1
+            while needed == 0 and left <= right:
+                if right - left + 1 < ans_len:
+                    ans = [left, right]
+                    ans_len = right - left + 1
+                c2 = s[left]
+                count_s[c2] -= 1
+                if c2 in count_t and count_s[c2] < count_t[c2]:
+                    needed += 1     
+                left += 1
+        return "" if ans_len == float("inf") else s[ans[0] : ans[1] + 1]
