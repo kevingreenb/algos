9a831e19414175824a7aafb0d6ff2202d337524a
Kevin Green <kevin.e.green@hotmail.com>
2026-01-01 11:08:30 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -19,6 +19,6 @@ class Solution:
                 c2 = s[left]
                 left += 1
                 count_s[c2] -= 1
-                if c2 in count_t and count_s[c2] < count_t[c2]:
-                    needed += 1     
+                if c2 in count_t and count_t[c2] == count_s[c2] - 1:
+                    needed += 1
         return "" if ans_len == float("inf") else s[ans[0] : ans[1] + 1]
