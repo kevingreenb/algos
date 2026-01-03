adcb80e5ae72bf76ea74d6b6c6c47ad103d78254
Kevin Green <kevin.e.green@hotmail.com>
2026-01-03 12:30:04 -0600

    Time: 21 ms (57.91%), Space: 18.1 MB (76.79%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,17 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
         def explore(r, c):
-            if r < 0 or c < 0 or r >= len(grid) or c >= len(grid[r]) or grid[r][c] != 1:
-                return 0
-            grid[r][c] = 0
-            ans = 1
-            directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            for r2, c2 in directions:
-                ans += explore(r + r2, c + c2)
-            return ans
+            if 0 <= r < m and 0 <= c < n and grid[r][c] == 1:
+                grid[r][c] = 0
+                ans = 1
+                directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+                for r2, c2 in directions:
+                    ans += explore(r + r2, c + c2)
+                return ans
+            return 0
         ans = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
+        for r in range(m):
+            for c in range(n):
                 if grid[r][c] == 1:
                     ans = max(ans, explore(r, c))
         return ans
