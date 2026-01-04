9c9387841d960beb79c82c8f4558127f456b77a6
Kevin Green <kevin.e.green@hotmail.com>
2026-01-04 14:16:43 -0600

    Time: 123 ms (82.7%), Space: 18.4 MB (98.62%) - LeetHub

---
+++ 1390-four-divisors/1390-four-divisors.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumFourDivisors(self, nums: list[int]) -> int:
+        total_sum = 0
+        
+        for num in nums:
+            divisors = set()
+            for i in range(1, int(num**0.5) + 1):
+                if num % i == 0:
+                    divisors.add(i)
+                    divisors.add(num // i)
+                    if len(divisors) > 4:
+                        break
+            if len(divisors) == 4:
+                total_sum += sum(divisors)
+                
+        return total_sum
