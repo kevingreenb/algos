e6c2b3c0a8570a97fdb7a455117cf07cabfdc3c9
Kevin Green <kevin.e.green@hotmail.com>
2026-02-11 19:29:22 -0600

    Time: 11 ms (75.72%), Space: 19.3 MB (60.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,9 +2,11 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        count = Counter(s)
+        count = defaultdict(int)
+        for c in s:
+            count[c] += 1
         for c in t:
-            if c not in count or count[c] == 0:
+            if count[c] == 0:
                 return False
             count[c] -= 1
         return True
