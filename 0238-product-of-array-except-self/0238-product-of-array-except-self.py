adb32d9ccc3de86e237c34f0d792147a03bff99b
Kevin Green <kevin.e.green@hotmail.com>
2026-02-12 07:08:35 -0600

    Time: 28 ms (38.5%), Space: 25.5 MB (49.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -3,11 +3,13 @@ class Solution:
         # [a, b, c, d]
         # [1, a, ab, abc]
         # [bcd,acd,abd,abc]
-        ans = [1] * len(nums)
-        for i in range(1, len(nums)):
+        n = len(nums)
+        ans = [1] * n
+        for i in range(1, n):
             ans[i] = ans[i - 1] * nums[i - 1]
         multiplier = 1
-        for i in range(len(nums)-1, -1, -1):
+        for i in range(n-1, -1, -1):
             ans[i] *= multiplier
             multiplier *= nums[i]
         return ans
+        
