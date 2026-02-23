fc37a8f4b3657bf1963360b5881903cf1b83955e
Kevin Green <kevin.e.green@hotmail.com>
2026-02-23 06:25:44 -0600

    Time: 67 ms (38.06%), Space: 23 MB (15.61%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -13,6 +13,8 @@ class Solution:
             return root
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
+
         if left and right:
             return root
+
         return left if left else right
