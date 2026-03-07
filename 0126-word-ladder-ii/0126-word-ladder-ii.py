db8a6f257d7ca44b70164b1287049312d4b89535
Kevin Green <kevin.e.green@hotmail.com>
2026-03-07 12:34:10 -0600

    Time: 7 ms (95.73%), Space: 19.9 MB (54.97%) - LeetHub

--- 0126-word-ladder-ii/0126-word-ladder-ii.py
+++ 0126-word-ladder-ii/0126-word-ladder-ii.py
@@ -1,53 +1,57 @@
-from collections import deque, defaultdict
+from collections import defaultdict, deque
+from typing import List
 
 class Solution:
-    def findLadders(self, beginWord: str, endWord: str, wordList: list[str]) -> list[list[str]]:
-        wordSet = set(wordList)
-        if endWord not in wordSet:
+    def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
+        if endWord not in wordList:
             return []
-
-        adj = defaultdict(list)
-        dist = {beginWord: 0}
-        wordSet.discard(beginWord)
         
-        queue = deque([beginWord])
+        wordList = set(wordList)
+        if beginWord not in wordList:
+            wordList.add(beginWord)
+        
+        g = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i+1:]
+                g[pattern].append(word)
+        
+        dist = {beginWord: 0}
+        parent = defaultdict(list)
+        q = deque([beginWord])
         found = False
         
-        while queue and not found:
-            current_level_visited = set()
-            for _ in range(len(queue)):
-                curr = queue.popleft()
+        while q and not found:
+            size = len(q)
+            for _ in range(size):
+                curr = q.popleft()
+                curr_dist = dist[curr]
+                
+                if curr == endWord:
+                    found = True
+                    continue
                 
                 for i in range(len(curr)):
-                    for char in "abcdefghijklmnopqrstuvwxyz":
-                        next_word = curr[:i] + char + curr[i+1:]
-                        
-                        if next_word in wordSet:
-                            if next_word not in dist:
-                                dist[next_word] = dist[curr] + 1
-                                current_level_visited.add(next_word)
-                                queue.append(next_word)
-
-                            if dist[next_word] == dist[curr] + 1:
-                                adj[next_word].append(curr)
-                                
-                            if next_word == endWord:
-                                found = True
-            
-            for w in current_level_visited:
-                wordSet.discard(w)
+                    pattern = curr[:i] + "*" + curr[i+1:]
+                    for neighbor in g[pattern]:
+                        if neighbor not in dist:
+                            dist[neighbor] = curr_dist + 1
+                            parent[neighbor].append(curr)
+                            q.append(neighbor)
+                        elif dist[neighbor] == curr_dist + 1:
+                            parent[neighbor].append(curr)
         
-        if not found:
+        if endWord not in dist:
             return []
+        
+        def backtrack(word):
+            if word == beginWord:
+                return [[beginWord]]
             
-        res = []
-        def dfs(curr, path):
-            if curr == beginWord:
-                res.append(path[::-1])
-                return
-            
-            for neighbor in adj[curr]:
-                dfs(neighbor, path + [neighbor])
-
-        dfs(endWord, [endWord])
-        return res
+            paths = []
+            for prev in parent[word]:
+                for path in backtrack(prev):
+                    paths.append(path + [word])
+            return paths
+        
+        return backtrack(endWord)
