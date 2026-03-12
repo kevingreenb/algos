e180e5529ec0d860120c00a239a883070851125b
Kevin Green <kevin.e.green@hotmail.com>
2026-03-12 05:40:01 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -7,28 +7,22 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.tree = root
-        self.inorder = []
-        self.current = 0
-        stack = []
-        node = root
-        
-        while stack or node:
+            self.stack = []
+            self._leftmost_inorder(root)
+
+        def _leftmost_inorder(self, node):
             while node:
-                stack.append(node)
+                self.stack.append(node)
                 node = node.left
-            node = stack.pop()
-            self.inorder.append(node.val)
-            node = node.right
-        
 
-    def next(self) -> int:
-        ans = self.inorder[self.current]
-        self.current += 1
-        return ans
+        def next(self) -> int:
+            top_node = self.stack.pop()
+            if top_node.right:
+                self._leftmost_inorder(top_node.right)
+            return top_node.val
 
-    def hasNext(self) -> bool:
-        return self.current < len(self.inorder)
+        def hasNext(self) -> bool:
+            return len(self.stack) > 0
         
 
 
