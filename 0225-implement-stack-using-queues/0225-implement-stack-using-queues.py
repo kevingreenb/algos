567bbab3d6a834fac9ea6454b19202cb7c1497c9
Kevin Green <kevin.e.green@hotmail.com>
2026-03-13 12:51:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,38 +1,26 @@
 class Node:
-    def __init__(self, val, next=None, prev=None):
+    def __init__(self, val: int, next_node: 'Node' = None):
         self.val = val
-        self.next = None
-        self.prev = None
+        self.next = next_node
 
 class MyStack:
-
-
     def __init__(self):
-        self.tail = Node(0)
+        self.head = None
 
     def push(self, x: int) -> None:
-        new_node = Node(x)
-        new_node.prev = self.tail
-        self.tail.next = new_node
-        self.tail = self.tail.next
+        self.head = Node(x, self.head)
 
     def pop(self) -> int:
-        value = self.top()
-        self.tail = self.tail.prev
-        self.tail.next = None
-        return value
+        if self.empty():
+            raise IndexError("pop from empty stack")
+        val = self.head.val
+        self.head = self.head.next
+        return val
 
     def top(self) -> int:
-        return self.tail.val
+        if self.empty():
+            raise IndexError("top from empty stack")
+        return self.head.val
 
     def empty(self) -> bool:
-        return self.tail.prev is None
-        
-
-
-# Your MyStack object will be instantiated and called as such:
-# obj = MyStack()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.empty()
+        return self.head is None
