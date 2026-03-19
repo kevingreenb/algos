50401c88eb1af32c8cdf61d25a3083dd3eae6a36
Kevin Green <kevin.e.green@hotmail.com>
2026-03-19 07:05:56 -0500

    Time: 23 ms (82.92%), Space: 24.1 MB (91.02%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,4 +1,5 @@
 class MyHashSet:
+
     def __init__(self):
         self.key_space = 2069
         self.buckets = [[] for _ in range(self.key_space)]
@@ -7,18 +8,19 @@ class MyHashSet:
         return key % self.key_space
 
     def add(self, key: int) -> None:
-        hash_key = self._hash(key)
-        if key not in self.buckets[hash_key]:
-            self.buckets[hash_key].append(key)
+        if not self.contains(key):
+            hashed_key = self._hash(key)
+            self.buckets[hashed_key].append(key)
 
     def remove(self, key: int) -> None:
-        hash_key = self._hash(key)
-        if key in self.buckets[hash_key]:
-            self.buckets[hash_key].remove(key)
+         if self.contains(key):
+            hashed_key = self._hash(key)
+            self.buckets[hashed_key].remove(key)       
 
     def contains(self, key: int) -> bool:
-        hash_key = self._hash(key)
-        return key in self.buckets[hash_key]
+        hashed_key = self._hash(key)
+        return key in self.buckets[hashed_key]
+        
 
 
 # Your MyHashSet object will be instantiated and called as such:
