53f7f2bd829e70816784fc2399945ef126eb9388
Kevin Green <kevin.e.green@hotmail.com>
2026-03-29 09:38:19 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.py
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -1,21 +1,16 @@
 from collections import deque
 
+
 class RecentCounter:
+
     def __init__(self):
-        # Deque is perfect for sliding window problems
-        self.queue = deque()
+        self.list = deque()
 
     def ping(self, t: int) -> int:
-        # 1. Add the new timestamp
-        self.queue.append(t)
-        
-        # 2. Remove timestamps outside the [t - 3000, t] range
-        # This is O(1) on average because each element is added/removed once
-        while self.queue[0] < t - 3000:
-            self.queue.popleft()
-            
-        return len(self.queue)
-        
+        self.list.append(t)
+        while t - self.list[0] > 3000:
+            self.list.popleft()
+        return len(self.list)
 
 
 # Your RecentCounter object will be instantiated and called as such:
